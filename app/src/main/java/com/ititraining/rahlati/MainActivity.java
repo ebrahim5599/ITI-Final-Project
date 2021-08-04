176f9b400ddbeadb3e2f3806d3400110442e382c
abc <abc@example.com>
2021-08-04 14:48:58 +0200

    Test GitHub

--- app/src/main/java/com/ititraining/rahlati/MainActivity.java
+++ app/src/main/java/com/ititraining/rahlati/MainActivity.java
@@ -35,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                Intent n = new Intent(MainActivity.this,SetTripActivity.class);
-                startActivity(n);
+                Intent intent = new Intent(MainActivity.this,SetTripActivity.class);
+                startActivity(intent);
             }
         });
 
