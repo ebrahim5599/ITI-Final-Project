695e32c3a603b3622db0e0b6dd183331ba689e96
Marina <marinaatef412@gmail.com>
2021-08-18 02:34:27 +0200

    History poliline SDKmap in fragment

--- app/src/main/java/com/ititraining/rahlati/ui/history/HistoryMapFragment.java
+++ app/src/main/java/com/ititraining/rahlati/ui/history/HistoryMapFragment.java
@@ -1,55 +1,65 @@
 package com.ititraining.rahlati.ui.history;
 
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.ititraining.rahlati.R;
+import com.ititraining.rahlati.ui.history.DirectionHelper.TaskLoadedCallback;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.firestore.GeoPoint;
-import com.ititraining.rahlati.R;
-import com.ititraining.rahlati.ui.history.DirectionHelper.TaskLoadedCallback;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-public class MapActivity  extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
+public class HistoryMapFragment extends Fragment implements OnMapReadyCallback, TaskLoadedCallback {
     String start_point, end_point;
-    MapActivity(String  start_point,String end_point){
-    this.start_point=start_point;
-     this.end_point=end_point; }
     private GoogleMap mMap;
     private MarkerOptions place1, place2;
     private Polyline currentPolyline;
     Random random = new Random();
     int rand_color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
     List<MarkerOptions> markerOptionsList=new ArrayList<>();
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_container_history);
+    public HistoryMapFragment(String  start_point,String end_point){
+        this.start_point=start_point;
+        this.end_point=end_point;}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_history_map, container, false);
+
 
         place1 = new MarkerOptions().position(getLocationFromAddress(start_point));
         place2 = new MarkerOptions().position(getLocationFromAddress(end_point));
         markerOptionsList.add(place1);
         markerOptionsList.add(place2);
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFreg);
+      //  com.google.android.gms.maps.MapFragment  mapFragment = (com.google.android.gms.maps.MapFragment)getFragmentManager().findFragmentById(R.id.mapFreg);
+      //  SupportMapFragment mapFragment =  (SupportMapFragment) form.getSupportFragmentManager().findFragmentByTag();//SupportMapFragment.findFragmentById(R.id.mapFreg);//.newInstance();
+        //mapFragment.getMapAsync(this);
+        SupportMapFragment mapFragment = (SupportMapFragment)getFragmentManager().findFragmentById(R.id.mapFreg);
         mapFragment.getMapAsync(this);
+         return view;
     }
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
@@ -83,7 +93,7 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
 
     public LatLng getLocationFromAddress(String strAddress){
 
-        Geocoder coder = new Geocoder(this);
+        Geocoder coder = new Geocoder(getApplicationContext(), Locale.getDefault());
         List<Address> address;
         GeoPoint p1 = null;
         LatLng latLng = null;
@@ -102,7 +112,7 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
 
             double lat = p1.getLatitude();
             double lng = p1.getLongitude();
-             latLng = new LatLng(lat, lng);
+            latLng = new LatLng(lat, lng);
 
         } catch (IOException e) {
             e.printStackTrace();
