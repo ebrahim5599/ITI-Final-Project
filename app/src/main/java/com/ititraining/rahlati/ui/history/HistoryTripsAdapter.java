2dbeec6ec76e25d5792caef1f7a7128c7130ca87
Ebrahim Abd-Elaziz <77022852+ebrahim5599@users.noreply.github.com>
2021-08-19 18:13:53 +0200

    Revert "Marina"

--- app/src/main/java/com/ititraining/rahlati/ui/history/HistoryTripsAdapter.java
+++ app/src/main/java/com/ititraining/rahlati/ui/history/HistoryTripsAdapter.java
@@ -40,7 +40,7 @@ public class HistoryTripsAdapter extends ArrayAdapter<UpComingTrips> {
                     parent,false);
 
         }
-//MapActivity mapActivity =new MapActivity("Damietta,Qism Damietta,Damietta Desert,Damietta Governorate,Egypt","Harayah Raznah,Zagazig,Ash Sharqia Governoment,Egypt");
+//MapActivity mapActivity =new MapActivity();
         UpComingTrips upComingTrips = (UpComingTrips) getItem(position);
 
         TextView date = convertView.findViewById(R.id.date_view);
--- app/src/main/java/com/ititraining/rahlati/ui/history/MapActivity.java
+++ app/src/main/java/com/ititraining/rahlati/ui/history/MapActivity.java
@@ -1,25 +1,20 @@
 package com.ititraining.rahlati.ui.history;
 
-import android.content.Context;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.test.core.app.ApplicationProvider;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -27,7 +22,6 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.firestore.GeoPoint;
 import com.ititraining.rahlati.R;
-import com.ititraining.rahlati.ui.history.DirectionHelper.FetchURL;
 import com.ititraining.rahlati.ui.history.DirectionHelper.TaskLoadedCallback;
 
 import java.io.IOException;
@@ -35,23 +29,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
 
-public class MapActivity  extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
- /////////////////////////////////////////
- Context context = ApplicationProvider.getApplicationContext();
-// MapActivity(String  start_point,String end_point){
-//     this.start_point=start_point;
-//     this.end_point=end_point; }
-    String start_point="Damietta,Qism Damietta,Damietta Desert,Damietta Governorate,Egypt",
-    end_point= "Harayah Raznah,Zagazig,Ash Sharqia Governoment,Egypt";
-
-
-    LatLng  start_point_lat=getLocationFromAddress(context,start_point),
-            end_point_lat=getLocationFromAddress(context,end_point);
-  /////////////////////
- Button getDirection;
-float r=new Random().nextInt(360);
-
+    String start_point, end_point;
     private GoogleMap mMap;
     private MarkerOptions place1, place2;
     private Polyline currentPolyline;
@@ -67,23 +47,11 @@ float r=new Random().nextInt(360);
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history_map);
+        setContentView(R.layout.view_container_history);
 
-        getDirection = findViewById(R.id.btnGetDirection);
-        getDirection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new FetchURL(MapActivity.this) .execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving"); }});
-
-
-//        place1 = new MarkerOptions().position(new LatLng(27.658143, 85.3199503));
-//        place2 = new MarkerOptions().position(new LatLng(27.667491, 85.3208583));
-        place1 = new MarkerOptions().position(start_point_lat)
-                .icon(BitmapDescriptorFactory.defaultMarker(r));
-        place2 = new MarkerOptions().position(end_point_lat)
-                .icon(BitmapDescriptorFactory.defaultMarker(r));
-
-       markerOptionsList.add(place1);
+        place1 = new MarkerOptions().position(getLocationFromAddress(start_point));
+        place2 = new MarkerOptions().position(getLocationFromAddress(end_point));
+        markerOptionsList.add(place1);
         markerOptionsList.add(place2);
 //        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag);
 //        mapFragment.getMapAsync(this);
@@ -95,8 +63,7 @@ float r=new Random().nextInt(360);
         Log.d("mylog", "Added Markers");
         mMap.addMarker(place1);
         mMap.addMarker(place2);
-        mMap.addPolyline((new PolylineOptions()).add(start_point_lat,end_point_lat).width(5).color(rand_color).geodesic(true));
-
+        mMap.addPolyline((new PolylineOptions()).add(getLocationFromAddress(start_point), getLocationFromAddress(end_point)).width(5).color(rand_color).geodesic(true));
         showAllMarkers();
     }
 
@@ -120,47 +87,32 @@ float r=new Random().nextInt(360);
         currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
     }
 
+    public LatLng getLocationFromAddress(String strAddress) {
 
-    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-        // Mode
-        String mode = "mode=" + directionMode;
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + mode;
-        // Output format
-        String output = "json";
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/" + output +"?" + parameters + "&key=AIzaSyDzTHI4DXKFwrM0xAzwnI-Brz1_3UkcL7g" ;
-        return url;
-    }
-
-
-
-
-    public LatLng getLocationFromAddress(Context context, String strAddress) {
-
-        Geocoder coder = new Geocoder( context);
+        Geocoder coder = new Geocoder(this);
         List<Address> address;
-        LatLng p1 = null;
-
+        GeoPoint p1 = null;
+        LatLng latLng = null;
         try {
-            // May throw an IOException
             address = coder.getFromLocationName(strAddress, 5);
             if (address == null) {
                 return null;
             }
-
             Address location = address.get(0);
-            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+            location.getLatitude();
+            location.getLongitude();
 
-        } catch (IOException ex) {
+            p1 = new GeoPoint((double) (location.getLatitude() * 1E6),
+                    (double) (location.getLongitude() * 1E6));
 
-            ex.printStackTrace();
+
+            double lat = p1.getLatitude();
+            double lng = p1.getLongitude();
+            latLng = new LatLng(lat, lng);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return p1;
+        return latLng;
     }
 }
