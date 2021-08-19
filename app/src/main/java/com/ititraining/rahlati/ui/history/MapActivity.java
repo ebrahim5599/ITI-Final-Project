81d9be70883c13427be4cb4a80017686ff49098e
Marina <marinaatef412@gmail.com>
2021-08-19 18:15:18 +0200

    poliline and direction

--- app/src/main/java/com/ititraining/rahlati/ui/history/MapActivity.java
+++ app/src/main/java/com/ititraining/rahlati/ui/history/MapActivity.java
@@ -1,19 +1,12 @@
 package com.ititraining.rahlati.ui.history;
-
-import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.test.core.app.ApplicationProvider;
-
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,57 +18,49 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.firebase.firestore.GeoPoint;
 import com.ititraining.rahlati.R;
 import com.ititraining.rahlati.ui.history.DirectionHelper.FetchURL;
 import com.ititraining.rahlati.ui.history.DirectionHelper.TaskLoadedCallback;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
 public class MapActivity  extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
- /////////////////////////////////////////
- Context context = ApplicationProvider.getApplicationContext();
-// MapActivity(String  start_point,String end_point){
-//     this.start_point=start_point;
-//     this.end_point=end_point; }
-    String start_point="Damietta,Qism Damietta,Damietta Desert,Damietta Governorate,Egypt",
-    end_point= "Harayah Raznah,Zagazig,Ash Sharqia Governoment,Egypt";
 
-
-    LatLng  start_point_lat=getLocationFromAddress(context,start_point),
-            end_point_lat=getLocationFromAddress(context,end_point);
-  /////////////////////
- Button getDirection;
-float r=new Random().nextInt(360);
+ //String start_point="Damietta",end_point="Zagazig";
+    String start_point,end_point;
+    MapActivity(String  start_point,String end_point){
+         this.start_point=start_point;
+       this.end_point=end_point; }
+    //button to show the direction betwen the start state and the end  state .
+     Button getDirection;
+    //random variable to give each trip markers a different color.
+    float r=new Random().nextInt(360);
+    //map to put poliline , murkers ,and the direction on.
     private GoogleMap mMap;
+    //the markers that show the location of the start state and the end  state on the map.
     private MarkerOptions place1, place2;
+    //the Polyline between the start state and the end  state on the map.
     private Polyline currentPolyline;
-    Random random = new Random();
-    int rand_color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    //list to put on it the two markers.
     List<MarkerOptions> markerOptionsList=new ArrayList<>();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_map);
-
-        getDirection = findViewById(R.id.btnGetDirection);
+        //the button when pressed shows the Direction
+ getDirection = findViewById(R.id.btnGetDirection);
+ //the Click Listener of the button
         getDirection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new FetchURL(MapActivity.this) .execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving"); }});
-
-
-//        place1 = new MarkerOptions().position(new LatLng(27.658143, 85.3199503));
-//        place2 = new MarkerOptions().position(new LatLng(27.667491, 85.3208583));
-        place1 = new MarkerOptions().position(start_point_lat)
-                .icon(BitmapDescriptorFactory.defaultMarker(r));
-        place2 = new MarkerOptions().position(end_point_lat)
-                .icon(BitmapDescriptorFactory.defaultMarker(r));
-
+                //show the Direction
+            new FetchURL(MapActivity.this) .execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving"); }});
+//the place where the marker will appear
+        place1 = new MarkerOptions().position(getLatLngFromAddress(start_point)).icon(BitmapDescriptorFactory.defaultMarker(r));
+        place2 = new MarkerOptions().position(getLatLngFromAddress(end_point)).icon(BitmapDescriptorFactory.defaultMarker(r));
        markerOptionsList.add(place1);
         markerOptionsList.add(place2);
+        //creat  map fragment
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFreg);
         mapFragment.getMapAsync(this);
     }
@@ -83,13 +68,16 @@ float r=new Random().nextInt(360);
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
         mMap = googleMap;
         Log.d("mylog", "Added Markers");
         mMap.addMarker(place1);
         mMap.addMarker(place2);
-        mMap.addPolyline((new PolylineOptions()).add(start_point_lat,end_point_lat).width(5).color(rand_color).geodesic(true));
+        mMap.addPolyline((new PolylineOptions()).add(getLatLngFromAddress(start_point),getLatLngFromAddress(end_point)));
         showAllMarkers();
     }
+
+    // function to make markers appear
     private void showAllMarkers(){
         LatLngBounds.Builder builder=new LatLngBounds.Builder();
         for(MarkerOptions m:markerOptionsList){
@@ -108,9 +96,7 @@ float r=new Random().nextInt(360);
             currentPolyline.remove();
         currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
     }
-
-
-    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+   private String getUrl(LatLng origin, LatLng dest, String directionMode) {
         // Origin of route
         String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
         // Destination of route
@@ -126,68 +112,25 @@ float r=new Random().nextInt(360);
         return url;
     }
 
+//function to get the Latitude and Longitude of the eache the start state and the end  state
+private LatLng getLatLngFromAddress(String address) {
 
+    Geocoder geocoder = new Geocoder(getApplicationContext());
+    List<Address> addressList;
 
-
-    public LatLng getLocationFromAddress(Context context, String strAddress) {
-
-        Geocoder coder = new Geocoder( context);
-        List<Address> address;
-        LatLng p1 = null;
-
-        try {
-            // May throw an IOException
-            address = coder.getFromLocationName(strAddress, 5);
-            if (address == null) {
-                return null;
-            }
-
-            Address location = address.get(0);
-            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
-
-        } catch (IOException ex) {
-
-            ex.printStackTrace();
+    try {
+        addressList = geocoder.getFromLocationName(address, 1);
+        if (addressList != null) {
+            Address singleaddress = addressList.get(0);
+            LatLng latLng = new LatLng(singleaddress.getLatitude(), singleaddress.getLongitude());
+            return latLng;
+        } else {
+            return null;
         }
-
-        return p1;
+    } catch (Exception e) {
+        e.printStackTrace();
+        return null;
     }
 
-
-
-
-
-
-
-
-
-
-
-   /* public LatLng getLocationFromAddress(String strAddress){
-
-        Geocoder coder = new Geocoder(this);
-        List<Address> address;
-        GeoPoint p1 = null;
-        LatLng latLng = null;
-        try {
-            address = coder.getFromLocationName(strAddress,5);
-            if (address==null) {
-                return null;
-            }
-            Address location=address.get(0);
-            location.getLatitude();
-            location.getLongitude();
-
-            p1 = new GeoPoint((double) (location.getLatitude() * 1E6),
-                    (double) (location.getLongitude() * 1E6));
-
-
-            double lat = p1.getLatitude();
-            double lng = p1.getLongitude();
-             latLng = new LatLng(lat, lng);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return  latLng ; }*/
+}
 }
